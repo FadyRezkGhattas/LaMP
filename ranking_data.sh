b3bd28a4c64ea79868866ff0542e16af0cb863d7
Fady Rezk <24905782+FadyRezkGhattas@users.noreply.github.com>
2024-09-09 23:12:21 +0100

    add ranking_data script for ranking commands

---
+++ ranking_data.sh
@@ -0,0 +1,36 @@
+python3 rank_profiles.py --task 'LaMP-1' --input_data_addr ./data_raw/user/LaMP_1/train_questions.json
+python3 rank_profiles.py --task 'LaMP-1' --input_data_addr ./data_raw/time/LaMP_1/train_questions.json"
+python3 rank_profiles.py --task 'LaMP-1' --input_data_addr ./data_raw/user/LaMP_1/dev_questions.json  
+python3 rank_profiles.py --task 'LaMP-1' --input_data_addr ./data_raw/time/LaMP_1/dev_questions.json" 
+python3 rank_profiles.py --task 'LaMP-1' --input_data_addr ./data_raw/user/LaMP_1/test_questions.json 
+python3 rank_profiles.py --task 'LaMP-1' --input_data_addr ./data_raw/time/LaMP_1/test_questions.json" 
+python3 rank_profiles.py --task 'LaMP-2' --input_data_addr ./data_raw/user/LaMP_2/train_questions.json
+python3 rank_profiles.py --task 'LaMP-2' --input_data_addr ./data_raw/time/LaMP_2/train_questions.json" 
+python3 rank_profiles.py --task 'LaMP-2' --input_data_addr ./data_raw/user/LaMP_2/dev_questions.json  
+python3 rank_profiles.py --task 'LaMP-2' --input_data_addr ./data_raw/time/LaMP_2/dev_questions.json" 
+python3 rank_profiles.py --task 'LaMP-2' --input_data_addr ./data_raw/user/LaMP_2/test_questions.json 
+python3 rank_profiles.py --task 'LaMP-2' --input_data_addr ./data_raw/time/LaMP_2/test_questions.json"
+python3 rank_profiles.py --task 'LaMP-3' --input_data_addr ./data_raw/user/LaMP_3/train_questions.json
+python3 rank_profiles.py --task 'LaMP-3' --ranker 'recency' --input_data_addr ./data_raw/time/LaMP_3/train_questions.json"
+python3 rank_profiles.py --task 'LaMP-3' --input_data_addr ./data_raw/user/LaMP_3/dev_questions.json  
+python3 rank_profiles.py --task 'LaMP-3' --ranker 'recency' --input_data_addr ./data_raw/time/LaMP_3/dev_questions.json"
+python3 rank_profiles.py --task 'LaMP-3' --input_data_addr ./data_raw/user/LaMP_3/test_questions.json 
+python3 rank_profiles.py --task 'LaMP-3' --ranker 'recency' --input_data_addr ./data_raw/time/LaMP_3/test_questions.json"
+python3 rank_profiles.py --task 'LaMP-4' --input_data_addr ./data_raw/user/LaMP_4/train_questions.json
+python3 rank_profiles.py --task 'LaMP-4' --ranker 'recency' --input_data_addr ./data_raw/time/LaMP_4/train_questions.json"
+python3 rank_profiles.py --task 'LaMP-4' --input_data_addr ./data_raw/user/LaMP_4/dev_questions.json  
+python3 rank_profiles.py --task 'LaMP-4' --ranker 'recency' --input_data_addr ./data_raw/time/LaMP_4/dev_questions.json"
+python3 rank_profiles.py --task 'LaMP-4' --input_data_addr ./data_raw/user/LaMP_4/test_questions.json 
+python3 rank_profiles.py --task 'LaMP-4' --ranker 'recency' --input_data_addr ./data_raw/time/LaMP_4/test_questions.json"
+python3 rank_profiles.py --task 'LaMP-5' --ranker 'bm25' --input_data_addr ./data_raw/user/LaMP_5/train_questions.json
+python3 rank_profiles.py --task 'LaMP-5' --input_data_addr ./data_raw/time/LaMP_5/train_questions.json"
+python3 rank_profiles.py --task 'LaMP-5' --ranker 'bm25' --input_data_addr ./data_raw/user/LaMP_5/dev_questions.json  
+python3 rank_profiles.py --task 'LaMP-5' --input_data_addr ./data_raw/time/LaMP_5/dev_questions.json"
+python3 rank_profiles.py --task 'LaMP-5' --ranker 'bm25' --input_data_addr ./data_raw/user/LaMP_5/test_questions.json 
+python3 rank_profiles.py --task 'LaMP-5' --input_data_addr ./data_raw/time/LaMP_5/test_questions.json"
+python3 rank_profiles.py --task 'LaMP-7' --input_data_addr ./data_raw/user/LaMP_7/train_questions.json
+python3 rank_profiles.py --task 'LaMP-7' --input_data_addr ./data_raw/time/LaMP_7/train_questions.json"
+python3 rank_profiles.py --task 'LaMP-7' --input_data_addr ./data_raw/user/LaMP_7/dev_questions.json  
+python3 rank_profiles.py --task 'LaMP-7' --input_data_addr ./data_raw/time/LaMP_7/dev_questions.json"
+python3 rank_profiles.py --task 'LaMP-7' --input_data_addr ./data_raw/user/LaMP_7/test_questions.json 
+python3 rank_profiles.py --task 'LaMP-7' --input_data_addr ./data_raw/time/LaMP_7/test_questions.json"
