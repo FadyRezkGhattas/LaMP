81026619632f38955f5fc90206ba703ce0984309
Fady Rezk <24905782+FadyRezkGhattas@users.noreply.github.com>
2024-09-15 20:54:54 +0100

    add GeneralSeq2SeqProfileDataset: train directly on user profile

--- data/datasets.py
+++ data/datasets.py
@@ -52,6 +52,79 @@ def convert_to_hf_dataset(dataset, cache_dir):
             yield dataset[idx]
     return datasets.Dataset.from_generator(gen, cache_dir = cache_dir)
 
+def get_io_keys(task):
+    if task == "LaMP-1":
+        return None # TODO
+    elif task == "LaMP-2":
+        return 'description', 'tag'
+    elif task == "LaMP-3":
+        return 'text', 'score'
+    elif task == "LaMP-4":
+        return 'text', 'title'
+    elif task == "LaMP-5":
+        return 'abstract', 'title'
+    elif task == "LaMP-6":
+        return None # TODO
+    elif task == "LaMP-7":
+        return None # TODO
+
+class GeneralSeq2SeqProfileDataset(Dataset):
+    def __init__(self, task, create_prompt, val=False, user_id=None, data=None, data_addr=None) -> None:
+        """
+        Loads dataset for specified task and user.
+
+        Args:
+            - **task** (str) : The name of the task.
+            - **create_prompt** (function) : Function that creates a prompt for each sample.
+            - **val** (bool) : If True, return datapoints for user input; otherwise, return profile data for training.
+            - **user_id** (int) : ID of the user to load data from. Defaults to None.
+            - **data** (a list of dict) : Data to use for this dataset. If given, it's assumed to be a single user.
+            - **data_addr** (str) : Path to the data file.
+        
+        Returns:
+            A dictionary containing the loaded dataset and its metadata
+
+        Example usage:
+
+            >>> from data.datasets import GeneralSeq2SeqProfileDataset
+            >>> from prompts.prompts import create_prompt_generator as f
+            >>> dataset = GeneralSeq2SeqProfileDataset('LaMP-2', f, user_id=125, data_addr='./data_raw/user/LaMP_2/train_questions.json')
+        """
+        super().__init__()
+        self.task = task
+        self.create_prompt = create_prompt
+        self.val = val
+
+        assert (data is None and data_addr != '') or (data != '' and data_addr is None), "Either data or data_addr must not be empty."
+        if data_addr is not None:
+            assert user_id is not None, "User id must be provided when using data_addr."
+            with open(data_addr) as f:
+                data = json.load(f)
+            self.data = data[user_id]
+        elif data is not None:
+            self.data = data
+        self.i_key, self.o_key = get_io_keys(self.task)
+
+    def __getitem__(self, index):
+        if not self.val:
+            return {
+                "id" : self.data['profile'][index]['id'],
+                "source" : self.create_prompt(self.data['profile'][index][self.i_key], self.task),
+                "target" : self.data['profile'][index][self.o_key]
+            }
+        else:
+            return {
+                "id" : self.data['id'],
+                "source" : self.data['input'],
+                "target" : self.data['output']
+            }
+    
+    def __len__(self):
+        if not self.val:
+            return len(self.data['profile'])
+        else:
+            return 1
+
 class GeneralSeq2SeqDataset(Dataset):
 
     def __init__(self, data_addr, use_profile, task, create_prompt = None) -> None:
