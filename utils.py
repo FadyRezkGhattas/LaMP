3e2a448dad78aa8b00cf5143d5bb5d2b4baee9f1
Fady Rezk <24905782+FadyRezkGhattas@users.noreply.github.com>
2024-09-21 00:11:55 +0100

    add

---
+++ utils.py
@@ -0,0 +1,26 @@
+from transformers import Trainer
+import csv
+import os
+
+def opts_to_exp_name(opts):
+    return f"{opts.task}_nvt_{opts.num_virtual_tokens}_lr_{opts.learning_rate}_wd_{opts.weight_decay}_epochs_{opts.num_train_epochs}"
+
+class CSVLogger:
+    def __init__(self, output_dir, exp_name):
+        self.filepath = os.path.join(output_dir, exp_name)
+        
+
+    def log(self, trainer : Trainer):
+        eval_result = trainer.state.log_history[-2]
+        train_result = trainer.evaluate(trainer.train_dataset)
+        all_results = {**eval_result, **train_result}
+        all_results['train_profile_size'] = len(trainer.train_dataset)
+        all_results['eval_profile_size'] = len(trainer.eval_dataset)
+
+        if not os.path.exists(self.filepath):
+            with open(self.filepath, 'w', newline='') as csvfile:
+                w = csv.DictWriter(csvfile, fieldnames=all_results.keys())
+                w.writeheader()
+        with open(self.filepath, 'a', newline='') as csvfile:
+            w = csv.DictWriter(csvfile, fieldnames=all_results.keys())
+            w.writerow(all_results)
