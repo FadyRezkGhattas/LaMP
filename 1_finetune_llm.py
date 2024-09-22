0dfea5195a2713f461128081ac4c7b192bd36447
Fady Rezk <24905782+FadyRezkGhattas@users.noreply.github.com>
2024-09-23 00:15:01 +0100

    add continual finetuning on aggregated user profiles

---
+++ 1_finetune_llm.py
@@ -0,0 +1,115 @@
+import os
+import json
+import argparse
+from rich import print
+
+from transformers.data.data_collator import DataCollatorForSeq2Seq
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, Seq2SeqTrainer, Seq2SeqTrainingArguments
+
+from metrics.classification_metrics import create_metric_f1_accuracy
+from prompts.prompts import create_prompt_generator as create_prompt_generator_val
+from prompts.singular_prompts import create_prompt_generator as create_prompt_generator_train
+from data.datasets import GeneralSeq2SeqDataset, GeneralSeq2SeqProfilesDataset, get_all_labels, create_preprocessor, convert_to_hf_dataset
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--train_data", default="./data_raw/user/LaMP_2/train_questions_merged.json")
+parser.add_argument("--validation_data", default="./data_raw/user/LaMP_2/dev_questions_merged.json")
+parser.add_argument("--test_data", default="")
+parser.add_argument("--model_name", default='google/flan-t5-base')
+parser.add_argument("--task", default='LaMP-2')
+parser.add_argument("--output_dir", default='./experiments/LaMP-2/finetune_all_train_user_profiles')
+parser.add_argument("--generation_max_length", type = int, default = 128)
+parser.add_argument("--per_device_batch_size", type = int, default = 16)
+parser.add_argument("--learning_rate", type = float, default = 5e-5)
+parser.add_argument("--weight_decay", type = float, default = 0.0001)
+parser.add_argument("--num_train_epochs", type = int, default = 50)
+parser.add_argument("--lr_scheduler_type", default = "linear")
+parser.add_argument("--warmup_ratio", type = float, default = 0.05)
+parser.add_argument("--generation_num_beams", type = int, default = 4)
+parser.add_argument("--num_retrieved", type = int, default=4)
+parser.add_argument("--gradient_accumulation_steps", type = int, default = 1)
+parser.add_argument("--cache_dir", default = "./cache")
+
+
+if __name__ == "__main__":
+    opts = parser.parse_args()
+    os.makedirs(opts.output_dir, exist_ok=True)
+    with open(os.path.join(opts.output_dir, "hyperparameters.json"), 'w') as f:
+        json.dump(vars(opts), f)
+    
+    model = AutoModelForSeq2SeqLM.from_pretrained(opts.model_name, cache_dir=opts.cache_dir)
+    tokenizer = AutoTokenizer.from_pretrained(opts.model_name, cache_dir=opts.cache_dir)
+    collator = DataCollatorForSeq2Seq(tokenizer = tokenizer, model = model)
+
+    print("[bold magenta]Step 1: Loading data and metrics...[/bold magenta]")
+    task = opts.task
+    prompt_generator_train = create_prompt_generator_train(tokenizer)
+    prompt_generator_val, _ = create_prompt_generator_val(0, tokenizer=tokenizer)
+
+    greater_is_better = True
+    if task == "LaMP-2":
+        train_dataset, labels = GeneralSeq2SeqProfilesDataset(task, prompt_generator_train, data_addr=opts.train_data), get_all_labels(task)
+        eval_dataset = GeneralSeq2SeqDataset(opts.validation_data, True, task, prompt_generator_val)
+        if opts.test_data:
+            test_dataset = GeneralSeq2SeqDataset(opts.test_data, opts.use_profile, task, prompt_generator_val)
+        compute_metrics = create_metric_f1_accuracy(tokenizer = tokenizer, all_labels = labels)
+        best_metric = "accuracy"
+    else:
+        raise ValueError(f"Task {task} not supported")
+    
+    print("[bold magenta]Step 2: Preprocessing data...[/bold magenta]")
+    print("Processing train data")
+    print(f"Train Split Size: {len(train_dataset)}")
+    train_dataset = convert_to_hf_dataset(train_dataset, cache_dir = opts.cache_dir).map(create_preprocessor(tokenizer = tokenizer, max_length = opts.generation_max_length), batched=True)
+    print("Processing eval data")
+    print(f"Eval Split Size: {len(eval_dataset)}")
+    eval_dataset = convert_to_hf_dataset(eval_dataset, cache_dir = opts.cache_dir).map(create_preprocessor(tokenizer = tokenizer, max_length = opts.generation_max_length), batched=True)
+    if opts.test_data:
+        test_dataset = convert_to_hf_dataset(test_dataset, cache_dir = opts.cache_dir).map(create_preprocessor(tokenizer = tokenizer, max_length = opts.generation_max_length), batched=True)
+    
+    print("[bold magenta]Step 3: Preparing training arguments and launching experiment[/bold magenta]")
+    training_args = Seq2SeqTrainingArguments(
+        output_dir = opts.output_dir,
+        do_train = True,
+        do_eval = True,
+        evaluation_strategy = "epoch",
+        per_device_train_batch_size = opts.per_device_batch_size,
+        per_device_eval_batch_size = opts.per_device_batch_size,
+        gradient_accumulation_steps = opts.gradient_accumulation_steps,
+        learning_rate = opts.learning_rate,
+        weight_decay = opts.weight_decay,
+        num_train_epochs = opts.num_train_epochs,
+        lr_scheduler_type = opts.lr_scheduler_type,
+        warmup_ratio = opts.warmup_ratio,
+        generation_num_beams = opts.generation_num_beams,
+        predict_with_generate = True,
+        save_strategy = "epoch",
+        logging_steps = 50,
+        eval_accumulation_steps = 1,
+        generation_max_length = opts.generation_max_length,
+        load_best_model_at_end = True,
+        metric_for_best_model = best_metric,
+        greater_is_better = greater_is_better,
+        save_total_limit=1,
+        save_steps=50
+    )
+
+    trainer = Seq2SeqTrainer(
+        model = model,
+        args = training_args,
+        data_collator = collator,
+        train_dataset = train_dataset,
+        eval_dataset = eval_dataset,
+        tokenizer = tokenizer,
+        compute_metrics = compute_metrics
+    )
+
+    trainer.train()
+
+    if opts.test_data:
+        results = trainer.evaluate(test_dataset)
+        print(results)
+
+        with open(os.join(opts.output_dir,'results_output.json'), 'w') as file:
+            json.dump(results, file, indent = 4)
