7382d57edd5dcb175c2a2b715725b16cb83480f5
Fady Rezk <24905782+FadyRezkGhattas@users.noreply.github.com>
2024-09-25 09:40:47 +0100

    add prompts for tasks 2 to 5

--- prompts/singular_prompts.py
+++ prompts/singular_prompts.py
@@ -1,5 +1,12 @@
-def create_classification_movies_prompt(inp, tokenizer):
-    prompt = "Which tag does this movie relate to among the following tags? Just answer with the tag name without further explanation. tags: [sci-fi, based on a book, comedy, action, twist ending, dystopia, dark comedy, classic, psychology, fantasy, romance, thought-provoking, social commentary, violence, true story] description:"
+prompts = {
+    'LaMP-2': 'Which tag does this movie relate to among the following tags? Just answer with the tag name without further explanation. tags: [sci-fi, based on a book, comedy, action, twist ending, dystopia, dark comedy, classic, psychology, fantasy, romance, thought-provoking, social commentary, violence, true story] description:',
+    'LaMP-3': 'What is the score of the following review on a scale of 1 to 5? just answer with 1, 2, 3, 4, or 5 without further explanation. review:',
+    'LaMP-4': 'Generate a headline for the following article:',
+    'LaMP-5': 'Generate a title for the following abstract of a paper:',
+}
+
+def create_prompt(inp, tokenizer, task):
+    prompt = prompts[task]
     inp = prompt + " " + inp
     max_in_len = tokenizer.max_model_input_sizes["t5-base"]
     tokens = tokenizer(inp, max_length=max_in_len, truncation=True)
@@ -8,6 +15,5 @@ def create_classification_movies_prompt(inp, tokenizer):
 
 def create_prompt_generator(tokenizer):
     def prompt(inp : str, task : str):
-        if task == "LaMP-2":
-            return create_classification_movies_prompt(inp, tokenizer)
+        return create_prompt(inp, tokenizer, task)
     return prompt
