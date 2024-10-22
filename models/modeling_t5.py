161e501197796d5392dc50f9c6aa37c2571f2a9b
Fady Rezk <24905782+FadyRezkGhattas@users.noreply.github.com>
2024-10-22 04:43:29 +0100

    detach only if computing loss with provided labels

--- models/modeling_t5.py
+++ models/modeling_t5.py
@@ -1812,8 +1812,8 @@ class T5ForConditionalGeneration(T5PreTrainedModel):
                 loss_fct = CrossEntropyLoss(ignore_index=-100)
                 # move labels to correct device to enable PP
                 labels = labels.to(lm_logits.device)
-                loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1))
-            losses.append(loss.detach().clone().item())
+                loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1)).detach().clone().item()
+            losses.append(loss)
                 # TODO(thom): Add z_loss https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/layers.py#L666
 
         if not return_dict:
