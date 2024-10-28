2b0ccf6299439c122efaa71ecd311348710fd202
Fady Rezk <24905782+FadyRezkGhattas@users.noreply.github.com>
2024-10-28 16:39:06 +0000

    return losses as a tensor

--- models/modeling_t5.py
+++ models/modeling_t5.py
@@ -1805,15 +1805,15 @@ class T5ForConditionalGeneration(T5PreTrainedModel):
             self.num_adapters = 1
         
         loss = None
-        losses=[]
+        losses=None
         lm_logits_reshaped = lm_logits.reshape(self.num_adapters, -1, *lm_logits.shape[1:])
         for lm_logits in lm_logits_reshaped:
             if labels is not None:
                 loss_fct = CrossEntropyLoss(ignore_index=-100)
                 # move labels to correct device to enable PP
                 labels = labels.to(lm_logits.device)
-                loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1)).detach().clone().item()
-            losses.append(loss)
+                loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1))
+                losses = torch.cat((losses, loss.view(1))) if losses is not None else loss.view(1)
                 # TODO(thom): Add z_loss https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/layers.py#L666
 
         if not return_dict:
