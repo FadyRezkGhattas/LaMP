595ee4be2223fc579b0d7cb42aedba0d92d1e45f
FadyRezkGhattas <24905782+FadyRezkGhattas@users.noreply.github.com>
2025-01-09 19:29:45 +0000

    add parent dir

--- eval/evaluate_adapters.py
+++ eval/evaluate_adapters.py
@@ -1,4 +1,9 @@
 import os
+import sys
+current_directory = os.path.dirname(os.path.realpath(__file__))
+parent_directory = os.path.dirname(current_directory)
+sys.path.append(parent_directory)
+
 import yaml
 import json
 import argparse
